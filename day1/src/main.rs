01039bd50fc60da7c00054e8c3a852d8650606dc
Miika Leinonen <miika@uprightproject.com>
2023-12-01 16:29:59 +0200

    Day 1 part 2

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,3 +1,4 @@
+use regex::Regex;
 use std::{
     fs::File,
     io::{BufRead, BufReader},
@@ -16,18 +17,35 @@ fn main() {
 
     let mut sum = 0;
 
+    let first_re =
+        Regex::new(r"([[:digit:]]|one|two|three|four|five|six|seven|eight|nine)").unwrap();
+    let last_re =
+        Regex::new(r"^.*([[:digit:]]|one|two|three|four|five|six|seven|eight|nine)").unwrap();
+
     for line in reader.lines() {
-        let line_numbers: Vec<_> = line
-            .expect("Reading a line failed.")
-            .chars()
-            .filter_map(|char| char.to_digit(10))
-            .collect();
+        let line = line.unwrap();
+        let first_match = &first_re.captures(&line).unwrap()[1];
+        let last_match = &last_re.captures(&line).unwrap()[1];
 
-        let first = line_numbers.first().unwrap();
-        let last = line_numbers.last().unwrap();
-
-        sum += 10 * first + last;
+        let value = 10 * string_to_value(first_match) + string_to_value(last_match);
+        println!("{}", value);
+        sum += value;
     }
 
     println!("Total calibration value: {}", sum);
+}
+
+fn string_to_value(str: &str) -> u32 {
+    match str {
+        "one" => 1,
+        "two" => 2,
+        "three" => 3,
+        "four" => 4,
+        "five" => 5,
+        "six" => 6,
+        "seven" => 7,
+        "eight" => 8,
+        "nine" => 9,
+        digit => digit.chars().next().unwrap().to_digit(10).unwrap(),
+    }
 }
